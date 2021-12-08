04f9fdc9ee57e9f690d07d0ccb764c3f8775862d
= <=>
2021-12-07 23:55:26 -0800

    Funcines basicas

---
+++ Column.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudSQLite
+{
+    public class Column
+    {
+        private string Name;
+        private dynamic Value;
+
+        public Column(string Name, dynamic Value)
+        {
+            this.Name = Name;
+            this.Value = Value;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public dynamic GetValue()
+        {
+            return Value;
+        }
+    }
+}
--- Crud.cs
+++ Crud.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SQLite;
 using System.Data;
+using System.Collections;
+using System.Globalization;
+using System.IO;
 
 namespace CrudSQLite
 {
@@ -17,12 +20,45 @@ namespace CrudSQLite
         {
             this.DATABASE_PATH = DATABASE_PATH; // Agregamos el Path
             Connect(); // Conectamos a la base de datos
+            InitPath();
+        }
+
+        private void InitPath()
+        {
+            if (!Directory.Exists("Scripts")) Directory.CreateDirectory("Scripts");
         }
 
         public void Connect()
         {
-            link = new SQLiteConnection(DATABASE_PATH); // Creamos la conexion
+            link = new SQLiteConnection("Data Source="+DATABASE_PATH); // Creamos la conexion
             link.Open(); // Abrimos la conexion
+        }
+
+        public void ExecuteScript(string script_name)
+        {
+            script_name += ".sql";
+            string script_path = Directory.GetCurrentDirectory() + "\\Scripts\\" + script_name;
+            if (File.Exists(script_path))
+            {
+                using(var fs = new FileStream(script_path, FileMode.Open, FileAccess.Read))
+                {
+                    using(var read = new StreamReader(fs))
+                    {
+                        string line = null;
+                        string command = "";
+                        while ((line = read.ReadLine()) != null)
+                        {
+                            command += line;
+                            if (line.Contains(";"))
+                            {
+                                command = command.Replace(";", "");
+                                CommandNoData(command);
+                                command = "";
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         public void CommandNoData(string cmd)
@@ -36,14 +72,80 @@ namespace CrudSQLite
             }
         }
 
-        public void CommandData(string cmd)
+        public DataTable CommandData(string cmd)
         {
+            DataTable datatable = new DataTable();
+            SQLiteDataAdapter adapter = null;
             if (link.State == ConnectionState.Open)
             {
                 using (var cmd_ = new SQLiteCommand(cmd, getLink()))
                 {
-                    cmd_.ExecuteReaderAsync();
+                    adapter = new SQLiteDataAdapter(cmd_);
+                    adapter.Fill(datatable);
                 }
+            }
+            return datatable;
+        }
+
+        public string InsertData(string tbname, Column[] columns)
+        {
+            try
+            {
+                string command_ = "INSERT INTO " + tbname + " (";
+                for (int i = 0; i < columns.Length; i++)
+                {
+                    if (i == columns.Length - 1) command_ += columns[i].GetName() + ") VALUES (";
+                    else command_ += columns[i].GetName() + ", ";
+                }
+                for (int i = 0; i < columns.Length; i++)
+                {
+                    string type = columns[i].GetValue().GetType().Name;
+                    if (i == columns.Length - 1)
+                    {
+                        if (type == "Int32" || type == "Int64" || type == "Double")
+                        {
+                            command_ += columns[i].GetValue().ToString(CultureInfo.CreateSpecificCulture("en-US")) + ")";
+                        }
+                        else
+                            command_ += "'" + columns[i].GetValue() + "')";
+                    }
+                    else
+                    {
+                        if (type == "Int32" || type == "Int64" || type == "Double")
+                        {
+                            command_ += columns[i].GetValue().ToString(CultureInfo.CreateSpecificCulture("en-US")) + ", ";
+                        }
+                        else
+                            command_ += "'" + columns[i].GetValue() + "', ";
+                    }
+                }
+                CommandNoData(command_);
+                return command_;
+            }
+            catch(SQLiteException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public DataTable GetDataById(string tbname, int id)
+        {
+            return CommandData("SELECT * FROM "+tbname+" WHERE _id=" + tbname.ToString());
+        }
+
+        public string DeleteData(string tbname, string condition)
+        {
+            try
+            {
+                string command_ = "DELETE FROM " + tbname + " WHERE " + condition;
+                CommandNoData(command_);
+                return command_;
+            }
+            catch(SQLiteException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
             }
         }
 
